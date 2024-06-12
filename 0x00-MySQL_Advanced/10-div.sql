810400b6cb84bbbb48fb2b8298aabee18beb18b1
rashaaz <rashaabdelaziz111@gmail.com>
2024-06-11 23:32:49 -0700

    add t10

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,15 @@
+-- Create an index named 'idx_name_first_score'
+DROP FUNCTION IF EXISTS SafeDiv;
+DELIMITER $$
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+    IF (b = 0)
+    THEN
+        RETURN (0);
+    ELSE
+        RETURN (a / b);
+    END IF;
+END
+$$ 
+DELIMITER ;
