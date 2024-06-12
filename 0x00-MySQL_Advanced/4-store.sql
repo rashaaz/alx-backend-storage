c301b68d1efe88956a42a95b48c74d08012038e9
rashaaz <rashaabdelaziz111@gmail.com>
2024-06-11 23:17:24 -0700

    add t4

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,5 @@
+-- Creates a trigger named buy_trigger
+CREATE TRIGGER buy_trigger
+AFTER INSERT ON orders
+FOR EACH ROW
+UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name;
