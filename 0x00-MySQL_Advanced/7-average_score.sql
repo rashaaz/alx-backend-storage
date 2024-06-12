016612e437b4c1c5b1c7b82c73af1f4dd297c390
rashaaz <rashaabdelaziz111@gmail.com>
2024-06-11 23:27:01 -0700

    add t7

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,12 @@
+--  Create a table named 'users' with the following schema
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id INT)
+BEGIN
+    DECLARE avg_score FLOAT;
+    SET avg_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
+    UPDATE users SET average_score = avg_score WHERE id=user_id;
+END
+$$
+DELIMITER ;
