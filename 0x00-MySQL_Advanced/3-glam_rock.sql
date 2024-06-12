0259bddbfe8af85e83f7ccacb7db164e94a236a2
rashaaz <rashaabdelaziz111@gmail.com>
2024-06-11 23:15:29 -0700

    add t3

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+--  Selects the band name and calculates
+SELECT band_name AS band_name, IFNULL(split, 2022) - IFNULL(formed, 0) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
